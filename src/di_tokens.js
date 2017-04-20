6bc79aea007ba91d0145a933a68f5e6504236f8c
Cédric Belin <cedric@belin.io>
2017-04-21 00:34:55 +0200

    Added the `WINDOW` injection token

---
+++ src/di_tokens.js
@@ -0,0 +1,7 @@
+import {InjectionToken} from '@angular/core';
+
+/**
+ * An injection token representing the browser's window.
+ * @type {InjectionToken}
+ */
+export const WINDOW = new InjectionToken('host.window');
