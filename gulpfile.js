d21c1329413ca19ce8c75c55f92f0d84e42bdafd
Cédric Belin <cedric@belin.io>
2017-04-20 20:56:24 +0200

    Added a build system based on Gulp.js

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const babel = require('gulp-babel');
+const child_process = require('child_process');
+const {david} = require('@cedx/gulp-david');
+const del = require('del');
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const path = require('path');
+
+/**
+ * Runs the default tasks.
+ */
+gulp.task('default', ['build']);
+
+/**
+ * Builds the client scripts.
+ */
+gulp.task('build', () => gulp.src('src/**/*.js')
+  .pipe(babel())
+  .pipe(gulp.dest('lib'))
+);
+
+/**
+ * Deletes all generated files and resets any saved state.
+ */
+gulp.task('clean', () => del('var/**/*'));
+
+/**
+ * Sends the results of the code coverage.
+ */
+gulp.task('coverage', ['test'], () => _exec('node_modules/.bin/coveralls', ['--file=var/lcov.info']));
+
+/**
+ * Builds the documentation.
+ */
+gulp.task('doc', async () => {
+  await del('doc/api');
+  return _exec('node_modules/.bin/esdoc');
+});
+
+/**
+ * Fixes the coding standards issues.
+ */
+gulp.task('fix', () => gulp.src(['*.js', 'src/**/*.js', 'test/**/*.js'], {base: '.'})
+  .pipe(eslint({fix: true}))
+  .pipe(gulp.dest('.'))
+);
+
+/**
+ * Performs static analysis of source code.
+ */
+gulp.task('lint', () => gulp.src(['*.js', 'src/**/*.js', 'test/**/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+);
+
+/**
+ * Checks the package dependencies.
+ */
+gulp.task('outdated', () => gulp.src('package.json').pipe(david()));
+
+/**
+ * Runs the unit tests.
+ */
+gulp.task('test', () => _exec('node_modules/.bin/nyc', [
+  '--report-dir=var',
+  '--reporter=lcovonly',
+  path.normalize('node_modules/.bin/mocha'),
+  '--compilers=js:babel-register',
+  '--recursive',
+  '--require=babel-polyfill'
+]));
+
+/**
+ * Spawns a new process using the specified command.
+ * @param {string} command The command to run.
+ * @param {string[]} [args] The command arguments.
+ * @param {object} [options] The settings to customize how the process is spawned.
+ * @return {Promise} Completes when the command is finally terminated.
+ */
+async function _exec(command, args = [], options = {shell: true, stdio: 'inherit'}) {
+  return new Promise((resolve, reject) => child_process
+    .spawn(path.normalize(command), args, options)
+    .on('close', code => code ? reject(new Error(`${command}: ${code}`)) : resolve())
+  );
+}
