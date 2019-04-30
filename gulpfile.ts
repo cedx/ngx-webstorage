b49db096a108192ad0b8b9120138dff0ebb39f1d
Cédric Belin <cedric@belin.io>
2019-04-30 23:36:52 +0200

    Ported the build system to TypeScript

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,8 +1,8 @@
-import {spawn} from 'child_process';
+import {spawn, SpawnOptions} from 'child_process';
 import del from 'del';
 import {promises} from 'fs';
-import gulp from 'gulp';
-import replace from 'gulp-replace';
+import * as gulp from 'gulp';
+import * as replace from 'gulp-replace';
 import {delimiter, normalize, resolve} from 'path';
 
 /**
@@ -16,7 +16,7 @@ const {dest, parallel, series, src, task, watch} = gulp;
 const {copyFile} = promises;
 
 // Initialize the build system.
-const _path = 'PATH' in process.env ? process.env.PATH : '';
+const _path = 'PATH' in process.env ? process.env.PATH! : '';
 const _vendor = resolve('node_modules/.bin');
 if (!_path.includes(_vendor)) process.env.PATH = `${_vendor}${delimiter}${_path}`;
 
@@ -73,28 +73,13 @@ task('default', task('build'));
 
 /**
  * Spawns a new process using the specified command.
- * @param {string} command The command to run.
- * @param {string[]} [args] The command arguments.
- * @param {SpawnOptions} [options] The settings to customize how the process is spawned.
- * @return {Promise<void>} Completes when the command is finally terminated.
+ * @param command The command to run.
+ * @param args The command arguments.
+ * @param options The settings to customize how the process is spawned.
+ * @return Completes when the command is finally terminated.
  */
-function _exec(command, args = [], options = {}) {
+function _exec(command: string, args: string[] = [], options: Partial<SpawnOptions> = {}): Promise<void> {
   return new Promise((fulfill, reject) => spawn(normalize(command), args, {shell: true, stdio: 'inherit', ...options})
     .on('close', code => code ? reject(new Error(`${command}: ${code}`)) : fulfill())
   );
 }
-
-/**
- * @typedef {object} SpawnOptions
- * @property {string} [argv0] Explicitly set the value of `argv[0]` sent to the child process.
- * @property {string} [cwd] Current working directory of the child process.
- * @property {boolean} [detached] Prepare child to run independently of its parent process.
- * @property {object} [env] Environment key-value pairs.
- * @property {number} [gid] Sets the group identity of the process.
- * @property {boolean|string} [shell] If `true`, runs command inside of a shell. A different shell can be specified as a string.
- * @property {Array|string} [stdio] Child's stdio configuration.
- * @property {number} [timeout] In milliseconds the maximum amount of time the process is allowed to run.
- * @property {number} [uid] Sets the user identity of the process.
- * @property {boolean} [windowsHide] Hide the subprocess console window that would normally be created on Windows systems.
- * @property {boolean} [windowsVerbatimArguments] No quoting or escaping of arguments is done on Windows.
- */
