f2296706f087a6d4d7d5f46a4b2daa2b03157287
Cédric Belin <cedric@belin.io>
2019-05-03 15:18:36 +0200

    Code optimization

--- etc/karma.js
+++ etc/karma.js
@@ -6,12 +6,7 @@ module.exports = config => config.set({
     reports: ['lcovonly']
   },
   frameworks: ['jasmine', '@angular-devkit/build-angular'],
-  plugins: [
-    require('karma-coverage-istanbul-reporter'),
-    require('karma-firefox-launcher'),
-    require('karma-jasmine'),
-    require('@angular-devkit/build-angular/plugins/karma')
-  ],
+  plugins: ['karma-*', '@angular-devkit/build-angular/plugins/karma'],
   reporters: ['progress'],
   singleRun: true
 });
