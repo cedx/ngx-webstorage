bdb8ac6114b39ad379bd1fd0f5fc99f26033901a
Cédric Belin <cedric@belin.io>
2019-06-04 22:35:10 +0200

    Added an example code

---
+++ example/main.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import {LocalStorage} from '@cedx/ngx-webstorage';
+
+/** A component that demonstrates the usage of the [[LocalStorage]] service. */
+@Component({
+  selector: 'my-component',
+  templateUrl: './my-component.html'
+})
+export class MyComponent implements OnInit {
+  constructor(private _storage: LocalStorage) {}
+
+  ngOnInit(): void {
+    console.log(this._storage.keys); // []
+    console.log(this._storage.length); // 0
+
+    this._storage.set('foo', 'bar');
+    console.log(this._storage.keys); // ["foo"]
+
+    this._storage.getObject('bar');
+
+    this._storage.set('foo', 'bar');
+    this._storage.setObject('foo', {bar: 'baz'});
+  }
+}
