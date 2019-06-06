d9d97ab8c31330581e10dce17861ed2977de4b20
Cédric Belin <cedric@belin.io>
2019-06-06 13:23:07 +0200

    Fixed the code coverage

--- gulpfile.ts
+++ gulpfile.ts
@@ -28,7 +28,7 @@ task('build', series('build:src', parallel('build:esm', 'build:types'), 'build:c
 task('clean', () => del(['build', 'doc/api', 'lib', 'var/**/*', 'web']));
 
 /** Uploads the results of the code coverage. */
-task('coverage:fix', () => src('var/lcov.info').pipe(replace(/\.js$/g, '.ts')).pipe(dest('var')));
+task('coverage:fix', () => src('var/lcov.info').pipe(replace(/\.js$/gm, '.ts')).pipe(dest('var')));
 task('coverage:upload', () => _exec('coveralls', ['var/lcov.info']));
 task('coverage', series('coverage:fix', 'coverage:upload'));
 
