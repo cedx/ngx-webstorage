44c27f945a747ea7a9186fae863f599d6768d2b9
Cédric Belin <cedric@belin.io>
2019-08-04 11:51:33 +0200

    Updated the example code

--- example/main.ts
+++ example/main.ts
@@ -12,16 +12,40 @@ export class MyComponent implements OnInit {
   constructor(private _storage: LocalStorage) {}
 
   ngOnInit(): void {
-    console.log(this._storage.keys); // []
+    // Query the storage.
+    console.log(this._storage.has('foo')); // false
+    console.log(this._storage.has('baz')); // false
     console.log(this._storage.length); // 0
+    console.log(this._storage.keys); // []
 
+    // Write to the storage.
     this._storage.set('foo', 'bar');
-    console.log(this._storage.keys); // ["foo"]
+    console.log(this._storage.has('foo')); // true
     console.log(this._storage.length); // 1
+    console.log(this._storage.keys); // ["foo"]
 
-    this._storage.getObject('bar'); // undefined
+    this._storage.setObject('baz', {qux: 123});
+    console.log(this._storage.has('baz')); // true
+    console.log(this._storage.length); // 2
+    console.log(this._storage.keys); // ["foo", "baz"]
 
-    this._storage.set('foo', 'bar');
-    this._storage.setObject('foo', {bar: 'baz'});
+    // Read the storage.
+    console.log(this._storage.get('foo').constructor.name); // "String"
+    console.log(this._storage.get('foo')); // "bar"
+
+    console.log(this._storage.getObject('baz').constructor.name); // "Object"
+    console.log(this._storage.getObject('baz')); // {qux: 123}
+    console.log(this._storage.getObject('baz').qux); // 123
+
+    // Delete from the storage.
+    this._storage.remove('foo');
+    console.log(this._storage.has('foo')); // false
+    console.log(this._storage.length); // 1
+    console.log(this._storage.keys); // ["baz"]
+
+    this._storage.clear();
+    console.log(this._storage.has('baz')); // false
+    console.log(this._storage.length); // 0
+    console.log(this._storage.keys); // []
   }
 }
