76e56db8bac135ece9dbbcdc5da2a9d539e21854
Cédric Belin <cedric@belin.io>
2020-03-29 02:14:28 +0100

    Updated the entry point of the tests

--- test/main.ts
+++ test/main.ts
@@ -1,13 +1,12 @@
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
-
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
 // Initialize the testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
-// Find all the tests.
+// Find all the tests and load the modules.
 declare const require: any;
 const context = require.context('.', true, /_test\.ts$/);
 context.keys().map(context);
