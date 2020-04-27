017e220d20d24aa9174071c3d1f2780131c22500
Cédric Belin <cedric@belin.io>
2020-04-27 12:43:09 +0200

    Dropped support for GitHub Packages

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,6 +1,5 @@
 const {spawn} = require('child_process');
 const del = require('del');
-const {promises} = require('fs');
 const {dest, parallel, series, src, task, watch} = require('gulp');
 const replace = require('gulp-replace');
 const {delimiter, normalize, resolve} = require('path');
@@ -41,9 +40,8 @@ task('fix', () => _exec('eslint', ['--config=etc/eslint.yaml', '--fix', 'src/**/
 task('lint', () => _exec('eslint', ['--config=etc/eslint.yaml', 'src/**/*.ts']));
 
 /** Publishes the package to the registry. */
-task('publish:github', () => _exec('npm', ['publish', '--registry=https://npm.pkg.github.com']));
 task('publish:npm', () => _exec('npm', ['publish', '--registry=https://registry.npmjs.org']));
-task('publish', series('clean', 'publish:github', 'publish:npm'));
+task('publish', series('clean', 'publish:npm'));
 
 /** Runs the test suites. */
 task('test', () => _exec('ng', ['test']));
